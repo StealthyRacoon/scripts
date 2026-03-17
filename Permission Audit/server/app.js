382a8aa7c230818a9b791b05c2735dcede44c089
Sarang Gadhiya <sarang.gadhiya@sttas.com.au>
2026-03-17 11:28:36 +1100

    audit.yml file

--- Permission Audit/server/app.js
+++ Permission Audit/server/app.js
@@ -5,13 +5,21 @@ const permissionsRoutes = require("./routes/permissionsRoutes");
 const superOwnersRoutes = require("./routes/superOwnersRoutes");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", permissionsRoutes);
 app.use("/api", superOwnersRoutes);
+
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
